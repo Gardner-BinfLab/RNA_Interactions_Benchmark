7e5e570764d2c72e40a3c8a537c911ff81e21eff
sinanugur <sinanugur@gmail.com>
2015-12-24 22:26:36 +1300

    Update

--- scripts/R-scripts/combined.boxplot.R
+++ scripts/R-scripts/combined.boxplot.R
@@ -31,10 +31,13 @@ pdf("overall.results.pdf",width = 12.1,height = 11)
 par(oma=c(10,4,2,2),mar=c(0,3,2,1),xpd=NA,cex.axis=1.1) #c(bottom, left, top, right)
 layout(matrix(c(1,1,2,2,3,3),nrow=3,byrow = T))
 boxplot(do.call(cbind,lapply(results_for_boxplot,function(df) return(df$V5))),names=F,col=Colors,ylim=c(-0.05,1.05)) #TPR
+mtext("A",side = 3,adj=0,cex=1.8,font=2)
 mtext("TPR (Sensitivity)",side=2,line=3,cex=1.8)
 boxplot(do.call(cbind,lapply(results_for_boxplot,function(df) return(df$V6))),names=F,col=Colors,ylim=c(-0.05,1.05)) #PPV
+mtext("B",side = 3,adj=0,cex=1.8,font=2)
 mtext("PPV (Precision)",side=2,line=3,cex=1.8)
 boxplot(do.call(cbind,lapply(results_for_boxplot,function(df) return(df$V7))),names=F,col=Colors,ylim=c(-0.05,1.05)) #MCC
+mtext("C",side = 3,adj=0,cex=1.8,font=2)
 mtext("MCC",side=2,line=3,cex=1.8)
 axis(1,at=1:14,cex.axis=1.8,las=2,labels=F)
 text(1:14,-0.3,labels=tools,cex=2,srt=45)
